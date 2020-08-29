d0a9b0d9c611ea38a35c9202f9f0c1a017319d21
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-29 03:09:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,14 +5,8 @@
 class EmailAddressParser
 attr_accessor :email 
  
-@@new_array = []
 def initialize(email)
-@email = email.split(' ')
-    @@new_array << email
-  end 
 
-def parse
-    @@new_array
   end 
 
 end 
