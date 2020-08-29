48a7f9a0ba4ccb759bb0c0e6da5db5b647637576
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-29 03:07:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -16,3 +16,19 @@
 #   end 
 
 # end 
+
+
+class EmailAddressParser
+
+attr_accessor(:email) 
+@@new_array = []
+
+def initialize(email)
+    @email = email.split(' ')
+    @@new_array << email
+end 
+
+def parse
+    @@new_array
+end 
+end 
