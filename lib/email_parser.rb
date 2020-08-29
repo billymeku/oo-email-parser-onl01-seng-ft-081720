beebb2cd96935df3b47a5e8a8c5a0a891ddd104c
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-29 03:08:44 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,19 +1,19 @@
-Build a class EmailAddressParser that accepts a string of unformatted 
-emails. The parse method on the class should separate them into
-unique email addresses. The delimiters to support are commas (',')
-or whitespace (' ').
-class EmailAddressParser
-attr_accessor :email 
+# Build a class EmailAddressParser that accepts a string of unformatted 
+# emails. The parse method on the class should separate them into
+# unique email addresses. The delimiters to support are commas (',')
+# or whitespace (' ').
+# class EmailAddressParser
+# attr_accessor :email 
  
-@@new_array = []
-def initialize(email)
-@email = email.split(' ')
-    @@new_array << email
-  end 
+# @@new_array = []
+# def initialize(email)
+# @email = email.split(' ')
+#     @@new_array << email
+#   end 
 
-def parse
-    @@new_array
-  end 
+# def parse
+#     @@new_array
+#   end 
 
-end 
+# end 
 
